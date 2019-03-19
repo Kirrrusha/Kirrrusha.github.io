0fa650724c0b3165df538026a24484a96ef91d07
Kirrrusha <kirilllebedenko@gmail.com>
2019-03-19 18:42:20 +0300

    Add files via upload

---
+++ precache-manifest.61f4928e30b803192ebe590c9b93ed41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "877fe15f6135c7a9cfb4",
+    "url": "/static/js/main.d7a07bf1.chunk.js"
+  },
+  {
+    "revision": "73d8585fb40bbc01c462",
+    "url": "/static/js/2.2bdf2d12.chunk.js"
+  },
+  {
+    "revision": "877fe15f6135c7a9cfb4",
+    "url": "/static/css/main.68e461a6.chunk.css"
+  },
+  {
+    "revision": "73d8585fb40bbc01c462",
+    "url": "/static/css/2.d33d9892.chunk.css"
+  },
+  {
+    "revision": "02216f15334caa0f124c8327d7cf7a2c",
+    "url": "/index.html"
+  }
+];
